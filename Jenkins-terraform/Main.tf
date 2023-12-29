986b845212907ab12333ea0226d84f2f90714932
pd1416 <113577261+pd1416@users.noreply.github.com>
2023-12-29 19:27:19 +0530

    Update Main.tf

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -59,9 +59,9 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0df4b2961410d4cff"
-  instance_type          = "t2.medium"
-  key_name               = "purplehaze"
+  ami                    = "ami-0c7217cdde317cfec"
+  instance_type          = "t2.large"
+  key_name               = "tetris"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
